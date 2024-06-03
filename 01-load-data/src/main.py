2d580e52f2f66ea01dccdccce7b31d9ce8929ffc
demenone <61589823+demenone@users.noreply.github.com>
2024-06-03 11:40:16 +0000

    commit final lab 1

--- 01-load-data/src/main.py
+++ 01-load-data/src/main.py
@@ -45,20 +45,118 @@ def list_blobs(account_name, container_name, credential):
 # Get content of blob
 def get_blob_content(account_name, container_name, blob_name, credential):
     print(f"Get content of blob {blob_name}")
+    try:
+        # Define the blob_service_client variable
+        blob_service_client = BlobServiceClient(
+            account_url=f"https://{account_name}.blob.core.windows.net",
+            credential=credential
+        )
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(blob_name)
+        blob_content = blob_client.download_blob().readall()
+        return blob_content
+    except Exception as e:
+        print(e)
 
 # Save content of blob to temp file
 def save_blob_to_temp_file(blob_content):
-    print(f"Save blob to temp file")
+    print(f"Save blob to temp file")    
+    with tempfile.NamedTemporaryFile(delete=False) as temp_pdf:
+        temp_pdf.write(blob_content)
+        temp_pdf_path = temp_pdf.name
+    return temp_pdf_path
+
 
 # Move blob to another container
 def move_blob(account_name, source_container_name, destination_container_name, blob_name, credential):
-    print(f"Moving blob '{blob_name}' from '{source_container_name}' to '{destination_container_name}'.")    
-
+    blob_service_client = BlobServiceClient(
+        account_url=f"https://{account_name}.blob.core.windows.net",
+        credential=credential
+    )
+    source_container_client = blob_service_client.get_container_client(source_container_name)
+    destination_container_client = blob_service_client.get_container_client(destination_container_name)
+    source_blob_client = source_container_client.get_blob_client(blob_name)
+    destination_blob_client = destination_container_client.get_blob_client(blob_name)
+    
+    destination_blob_client.start_copy_from_url(source_blob_client.url)
+    props = destination_blob_client.get_blob_properties()
+    while props.copy.status == "pending":
+        props = destination_blob_client.get_blob_properties()
+    
+    if props.copy.status != "success":
+        raise Exception(f"Copy failed with status: {props.copy.status}")
+    source_blob_client.delete_blob()
     print(f"Blob '{blob_name}' has been moved from '{source_container_name}' to '{destination_container_name}'.")
 
 # Proceed pdf blob with MapReduceDocumentsChain, use blob content as input
 def get_file_classification(credential, temp_pdf_path):
     print("Proceed pdf blob with MapReduceDocumentsChain, use blob content as input")
+    # Load the PDF file
+    loader = PyPDFLoader(temp_pdf_path)
+    docs = loader.load_and_split()
+    token_provider = get_bearer_token_provider(
+        credential, "https://cognitiveservices.azure.com/.default"
+    )
+
+    llm = AzureChatOpenAI(
+        openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+        azure_deployment=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
+        azure_ad_token_provider=token_provider
+    )
+
+    map_template = """The following is a set of documents
+    {docs}
+    Twoim zadaniem jest sklasyfikować dokument. Klasyfikacja dokumentów od najważniejszego do najniższego: Dane osobowe, Prywatny, Publiczny.
+    Użyj następujących etykiet: personal-data dla "Dane osobowe", private dla "Prywatny", oraz public dla "Publiczny"
+    Jeżeli nie jest możliwe sklasyfikowanie usatw etykietę `private`.
+    W odpowiedzi podaj tylko klasyfikację. Jeżeli nie jesteś pewien, użyj etykiety private.
+    Classification:"""
+    
+    map_prompt = PromptTemplate.from_template(map_template)
+
+    map_chain = LLMChain(llm=llm, prompt=map_prompt)
+
+    # Reduce
+    reduce_template = """Poniżej znajduje się klasyfikacja dokumentów:
+    {docs}
+    Twoim zadaniem jest wybrać jedną klasyfikację dla tego zestawu. Klasyfikacja dokumentów od najważniejszego do najniższego: Dane osobowe, Prywatny, Publiczny.
+    Użyj następujących etykiet: personal-data dla "Dane osobowe", private dla "Prywatny", oraz public dla "Publiczny"
+    W odpowiedzi podaj tylko klasyfikację. Jeżeli nie jesteś pewien, użyj etykiety private.
+    Classification:"""
+    reduce_prompt = PromptTemplate.from_template(reduce_template)
+
+    # Run chain
+    reduce_chain = LLMChain(llm=llm, prompt=reduce_prompt)
+
+    # Takes a list of documents, combines them into a single string, and passes this to an LLMChain
+    combine_documents_chain = StuffDocumentsChain(
+        llm_chain=reduce_chain, document_variable_name="docs"
+    )
+    # Combines and iteratively reduces the mapped documents
+    reduce_documents_chain = ReduceDocumentsChain(
+        # This is final chain that is called.
+        combine_documents_chain=combine_documents_chain,
+        # If documents exceed context for `StuffDocumentsChain`
+        collapse_documents_chain=combine_documents_chain,
+        # The maximum number of tokens to group documents into.
+        token_max=4000,
+    )
+    # Combining documents by mapping a chain over them, then combining results
+    map_reduce_chain = MapReduceDocumentsChain(
+        # Map chain
+        llm_chain=map_chain,
+        # Reduce chain
+        reduce_documents_chain=reduce_documents_chain,
+        # The variable name in the llm_chain to put the documents in
+        document_variable_name="docs",
+        # Return the results of the map steps in the output
+        return_intermediate_steps=False,
+    )
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    split_docs = text_splitter.split_documents(docs)
+    return map_reduce_chain.run(split_docs)
 
 def get_vector_store(credential, index_name):
     token_provider = get_bearer_token_provider(
@@ -102,6 +200,17 @@ def get_vector_store(credential, index_name):
 
 def add_document_to_vector_store(vector_store, file_path, data_classification, title, source):
     print("Add document '{title}' to vector store")
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    for doc in docs:
+        doc.metadata["data_classification"] = data_classification
+        doc.metadata["title"] = title
+        doc.metadata["source"] = source
+    vector_store.add_documents(
+        documents=docs,
+    )
 
 if __name__ == "__main__":
     load_dotenv()
@@ -110,17 +219,17 @@ if __name__ == "__main__":
     credential = DefaultAzureCredential()
     index_name = os.getenv("INDEX_NAME")
     # Przykładowa implementacja
-    vector_store = get_vector_store(credential, os.getenv("INDEX_NAME"))
-    # blobs = list_blobs(account_name, container_name, credential)
-    # for blob in blobs:
-    #     blob_content = get_blob_content(account_name, container_name, blob.name, credential)
-    #     file_path = save_blob_to_temp_file(blob_content)
-    #     data_classification = get_file_classification(credential, file_path)
-    #     add_document_to_vector_store(
-    #         vector_store=vector_store,
-    #         file_path=file_path,
-    #         data_classification=data_classification,
-    #         title=blob.name,
-    #         source=f"https://{account_name}.blob.core.windows.net/{data_classification}/{blob.name}"
-    #     )
-    #     move_blob(account_name, container_name, data_classification, blob.name, credential)
+    vector_store = get_vector_store(credential, index_name)
+    blobs = list_blobs(account_name, container_name, credential)
+    for blob in blobs:
+        blob_content = get_blob_content(account_name, container_name, blob.name, credential)
+        file_path = save_blob_to_temp_file(blob_content)
+        data_classification = get_file_classification(credential, file_path)
+        add_document_to_vector_store(
+            vector_store=vector_store,
+            file_path=file_path,
+            data_classification=data_classification,
+            title=blob.name,
+            source=f"https://{account_name}.blob.core.windows.net/{data_classification}/{blob.name}"
+        )
+        move_blob(account_name, container_name, data_classification, blob.name, credential)
